e4b69e7d2f6ad9ebab0f697d8d987981b9e36bbf
Victor Garcia <vimago81@gmail.com>
2019-04-02 15:53:05 +0200

    Label component

---
+++ components/atoms/label/Label.vue
@@ -0,0 +1,45 @@
+<template>
+  <label
+    class="label"
+    :style="theme"
+  >
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    customTheme: {
+      type: Object,
+      default: () => {},
+    },
+    customStyles: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data: function () {
+    const defaultStyles = {
+      color: '#333',
+      alignSelf: 'left',
+      padding: '10px',
+      fontFamily: 'inherit',
+      fontSize:'1em',
+      fontWeight: 'bold',
+      width: '100%',
+    };
+
+    return {
+      theme : {
+        ...defaultStyles,
+        ...this.customTheme,
+        ...this.customStyles,
+      }
+    };
+  },
+}
+</script>
+
+
+
