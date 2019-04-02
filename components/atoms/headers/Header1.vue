36da4060a9e01d5c888c597efbb3e01b4df4a819
Victor Garcia <vimago81@gmail.com>
2019-04-02 15:29:26 +0200

    H1 component created

---
+++ components/atoms/headers/Header1.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1
+    class="header1"
+    :style="theme"
+  >
+    <slot></slot>
+  </h1>
+</template>
+
+<script>
+export default {
+  props: {
+    customTheme: {
+      type: Object,
+      default: () => {},
+    },
+    customStyles: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data: function () {
+    const defaultStyles = {
+      color: '#333',
+      textAlign: 'center',
+      padding: '20px',
+      fontFamily: 'inherit',
+      fontSize:'3em',
+    };
+
+    return {
+      theme : {
+        ...defaultStyles,
+        ...this.customTheme,
+        ...this.customStyles,
+      }
+    };
+  },
+}
+</script>
+
+
+
---
+++ pages/showComponents.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Header1
+      :customTheme="theme1"
+    >
+      I am going to display the base components here
+    </Header1>
+    <Header1
+      :customTheme="theme2"
+      :customStyles="tupi"
+    >
+      Tupitupi
+    </Header1>
+  </div>
+</template>
+
+<script>
+import Header1 from '@/components/atoms/headers/Header1';
+export default {
+  components: { Header1 },
+  data: function () {
+    return {
+      theme1: {
+        color: 'green',
+        fontFamily: `'Laila', serif`,
+        fontSize:'4em',
+      },
+      theme2: {
+        textAlign: 'right',
+        padding: '50px',
+      },
+      tupi: {
+        color: 'red',
+        marginTop: '50px',
+      }
+    }
+  }
+}
+</script>
+
+
